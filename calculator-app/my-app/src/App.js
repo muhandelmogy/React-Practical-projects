95c4313bc00e03040cc0afefaf9c334861e7cd77
Muhand Elmogy <muhandelmogy259@gmail.com>
2023-06-18 08:48:35 +0300

    fixing the file name

---
+++ calculator-app/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+import './App.scss'; 
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
